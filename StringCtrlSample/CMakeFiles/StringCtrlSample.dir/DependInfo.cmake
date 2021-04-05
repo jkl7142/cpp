60c91fcc0f4f7f646f20a34c7c060a7c344562a9
RookKim <55984018+jkl7142@users.noreply.github.com>
2021-04-05 16:04:56 +0900

    Upload StringCtrlSample project

---
+++ StringCtrlSample/CMakeFiles/StringCtrlSample.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jkl41/vscode/cppstudy/StringCtrlSample/MyString.cpp" "CMakeFiles/StringCtrlSample.dir/MyString.cpp.obj" "gcc" "CMakeFiles/StringCtrlSample.dir/MyString.cpp.obj.d"
+  "C:/Users/jkl41/vscode/cppstudy/StringCtrlSample/StringCtrlSample.cpp" "CMakeFiles/StringCtrlSample.dir/StringCtrlSample.cpp.obj" "gcc" "CMakeFiles/StringCtrlSample.dir/StringCtrlSample.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ StringCtrlSample/build/CMakeFiles/StringCtrlSample.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StringCtrlSample.dir/MyString.cpp.obj"
+  "CMakeFiles/StringCtrlSample.dir/MyString.cpp.obj.d"
+  "CMakeFiles/StringCtrlSample.dir/StringCtrlSample.cpp.obj"
+  "CMakeFiles/StringCtrlSample.dir/StringCtrlSample.cpp.obj.d"
+  "StringCtrlSample.exe"
+  "StringCtrlSample.exe.manifest"
+  "StringCtrlSample.pdb"
+  "libStringCtrlSample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StringCtrlSample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
